e6d96c32f9d79e80b97c026fe313182ec0eb2bbd
jzh135 <163363154+jzh135@users.noreply.github.com>
2024-03-26 19:30:12 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -16,16 +16,14 @@ SOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'source')
 ARCHIVE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'archive')
 METADATA_DIRECTORY = os.path.join(ARCHIVE_DIRECTORY, 'metadata')
 ARCHIVE_SOURCE_DIRECTORY = os.path.join(ARCHIVE_DIRECTORY, 'source files')
+
 # Audio Directory
 AUDIO_ROOT = os.path.join(os.path.dirname(__file__), 'audio_docs')
 AUDIO_SOURCE = os.path.join(AUDIO_ROOT, 'audio_source')
 TRANSCRIPTION = os.path.join(AUDIO_ROOT, 'transcription')
+
 # Database Directory
 DB_DIRECTORY = os.path.join(os.path.dirname(__file__), 'db_en')
-
-# Embeddings Directory
-MODEL_ROOT = os.path.join(os.path.dirname(__file__), 'model')
-BEG_EN = os.path.join(MODEL_ROOT,'bge-large-en-v1.5')
 
 # OpenAI API Key 
 API_KEY_FILE = os.path.join(os.path.dirname(__file__), 'openAI_API.txt')
@@ -33,8 +31,9 @@ API_KEY = read_api_key_from_file(API_KEY_FILE)
 
 # Report Directory
 REPORT_DIRECTORY = os.path.join(os.path.dirname(__file__), 'report')
+
 # Check if essential directories exist
-for folder_path in [SOURCE_DIRECTORY, ARCHIVE_DIRECTORY, METADATA_DIRECTORY, ARCHIVE_SOURCE_DIRECTORY, AUDIO_ROOT, AUDIO_SOURCE, TRANSCRIPTION, DB_DIRECTORY, MODEL_ROOT]:
+for folder_path in [SOURCE_DIRECTORY, ARCHIVE_DIRECTORY, METADATA_DIRECTORY, ARCHIVE_SOURCE_DIRECTORY, AUDIO_ROOT, AUDIO_SOURCE, TRANSCRIPTION, DB_DIRECTORY, REPORT_DIRECTORY]:
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         print(f"Folder '{folder_path}' created successfully.")
